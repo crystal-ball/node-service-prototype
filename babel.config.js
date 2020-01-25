a36d6f1731de7dd02cc6c063755bb9da1b25c90c
Dan Hedgecock <Hedgecock.D@gmail.com>
2020-01-25 14:42:38 -0800

    Chore: Upgrade dependencies 🆙 (#57)

--- babel.config.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-module.exports = {
-  // Code coverage collector for Jest fails on class properties without the
-  // Babel transform 🤔
-  plugins: ['@babel/plugin-proposal-class-properties'],
-}
--- jest.config.js
+++ jest.config.js
@@ -19,5 +19,8 @@ module.exports = {
   coverageReporters: ['text-summary', 'lcov'],
   collectCoverageFrom: ['src/**/*.js'],
 
+  // Disable Babel transforms, Node has full support for all lanugage features used
+  transform: {},
+
   globalSetup: TEST_SUITE === 'acceptance' ? './test/setup/global-setup.js' : undefined,
 }
