34c7de5db1dc0c0ab4d7be9247f7e01060b66953
Dan Hedgecock <dan@lightstep.com>
2021-05-12 17:42:18 -0700

    Resource version

--- src/tracer.js
+++ src/tracer.js
@@ -7,17 +7,24 @@ const { NodeTracerProvider } = require('@opentelemetry/node')
 const { HttpInstrumentation } = require('@opentelemetry/instrumentation-http')
 const { ExpressInstrumentation } = require('@opentelemetry/instrumentation-express')
 const { CollectorTraceExporter } = require('@opentelemetry/exporter-collector')
+const { Resource, SERVICE_RESOURCE } = require('@opentelemetry/resources')
 const {
   BatchSpanProcessor,
   ConsoleSpanExporter,
   SimpleSpanProcessor,
 } = require('@opentelemetry/tracing')
 
+const { version } = require('../package.json')
+
 // eslint-disable-next-line node/no-process-env
 const { LS_ACCESS_TOKEN } = process.env
 
 // Create and register the tracer provider
-const provider = new NodeTracerProvider()
+const provider = new NodeTracerProvider({
+  resource: new Resource({
+    [SERVICE_RESOURCE.VERSION]: version,
+  }),
+})
 provider.register()
 
 // Create the span processor - export to LS if an access token is provided, otherwise
