6c395e12b3ae94c7c06010776131478acc20df20
Dan Hedgecock <hedgecock.d@gmail.com>
2021-05-19 05:57:44 -0700

    Run format

--- src/middleware/validation.js
+++ src/middleware/validation.js
@@ -24,36 +24,38 @@ addFormats(ajv)
  * ?? Would it be helpful to add some logging about the body here? It has the
  * danger of accidentally logging user info if it's forgetten about...
  */
-const requestValidation = ({ body, headers, queryParams }) => (req, res, next) => {
-  const errors = []
+const requestValidation =
+  ({ body, headers, queryParams }) =>
+  (req, res, next) => {
+    const errors = []
 
-  if (body) {
-    const validate = ajv.compile(body)
-    const valid = validate(req.body)
-    if (!valid) errors.push(...validate.errors)
+    if (body) {
+      const validate = ajv.compile(body)
+      const valid = validate(req.body)
+      if (!valid) errors.push(...validate.errors)
+    }
+
+    if (headers) {
+      const validate = ajv.compile(headers)
+      const valid = validate(req.headers)
+      if (!valid) errors.push(...validate.errors)
+    }
+
+    if (queryParams) {
+      const validate = ajv.compile(queryParams)
+      const valid = validate(req.query)
+      if (!valid) errors.push(...validate.errors)
+    }
+
+    if (!errors.length) return next() // Good news, this request is valid 🎉
+
+    return res.status(400).send({
+      error: {
+        code: 'INVALID_REQUEST',
+        message: 'Request is not valid',
+        errors,
+      },
+    })
   }
-
-  if (headers) {
-    const validate = ajv.compile(headers)
-    const valid = validate(req.headers)
-    if (!valid) errors.push(...validate.errors)
-  }
-
-  if (queryParams) {
-    const validate = ajv.compile(queryParams)
-    const valid = validate(req.query)
-    if (!valid) errors.push(...validate.errors)
-  }
-
-  if (!errors.length) return next() // Good news, this request is valid 🎉
-
-  return res.status(400).send({
-    error: {
-      code: 'INVALID_REQUEST',
-      message: 'Request is not valid',
-      errors,
-    },
-  })
-}
 
 module.exports = { requestValidation }
